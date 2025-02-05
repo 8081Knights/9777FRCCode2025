261b4a3866c2c8c738c6047b928c0633dffd2890
Knights8081 <Knights8081@gcsaz.org>
2025-02-04 18:48:14 -0700

    started SwerveDrive.java

--- src/main/java/frc/robot/teleopswerve/SwerveDrive.java
+++ src/main/java/frc/robot/teleopswerve/SwerveDrive.java
@@ -1,5 +1,22 @@
 package frc.robot.teleopswerve;
+import edu.wpi.first.wpilibj.XboxController;
+
 
 public class SwerveDrive {
+    public class ControllerVariables {
     
+        double[][] moduleError = new double[4][2];
+        double[] defaultAngles = new double[4];
+        double addictiveAngle;
+
+    }
+
+    /**
+     * Makes the robot move with the controller, unadvanced, no positioning or anything wanted.
+     * @param controller
+     */
+    public static void teleopDrive(XboxController controller) {
+
+        
+    }
 }
