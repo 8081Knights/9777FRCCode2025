c38ea153a4e916c417196350d7128a7418eef9ed
Knights8081 <Knights8081@gcsaz.org>
2025-02-06 16:34:00 -0700

    added some initialization (lots of work to do)

--- src/main/java/frc/robot/teleopswerve/SwerveDrive.java
+++ src/main/java/frc/robot/teleopswerve/SwerveDrive.java
@@ -5,9 +5,13 @@ import edu.wpi.first.wpilibj.XboxController;
 public class SwerveDrive {
     public class ControllerVariables {
     
-        double[][] moduleError = new double[4][2];
-        double[] defaultAngles = new double[4];
-        double addictiveAngle;
+        public static double[][] moduleError = new double[4][2];
+        public static double[] defaultAngles = new double[4];
+        public static double distancePerUnit = 3;
+        public static double additiveAngle;
+        public static double[] robotDimentions;
+        public static double hypotenuse;
+
 
     }
 
@@ -16,7 +20,18 @@ public class SwerveDrive {
      * @param controller
      */
     public static void teleopDrive(XboxController controller) {
-
         
+        
+    }
+
+    public static void init(double[] dimetions, double inputDefaultAngles[]) {
+        for (int i = 0; i < inputDefaultAngles.length; i++) {
+            ControllerVariables.defaultAngles[i] = inputDefaultAngles[i];
+            
+        }
+        ControllerVariables.robotDimentions = dimetions;
+        ControllerVariables.hypotenuse = Math.sqrt((dimetions[0] * dimetions[0]) + (dimetions[1] * dimetions[1]));
+
+
     }
 }
