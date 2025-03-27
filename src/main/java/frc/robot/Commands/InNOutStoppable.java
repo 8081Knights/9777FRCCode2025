78cd7128c3438b92259c90b7178d41c9f3a7951b
Knights8081 <Knights8081@gcsaz.org>
2025-03-26 19:34:50 -0700

    added functionality to innoutstoppable command, might not do anything/might break stuff

--- src/main/java/frc/robot/Commands/InNOutStoppable.java
+++ src/main/java/frc/robot/Commands/InNOutStoppable.java
@@ -24,7 +24,10 @@ public class InNOutStoppable extends Command {
     // Called once the command ends or is interrupted.
     
     @Override
-    public void end(boolean interrupted) {}
+    public void end(boolean interrupted) {
+        HardwareMappings.intakeOuttake.set(0);
+    }
+
 
     // Returns true when the command should end.
     @Override
